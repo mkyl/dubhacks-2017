08511be57268e8321ac06f927d1f0fb5ad258480
Robert Kolmos <robertk3@cs.washington.edu>
2017-10-22 10:59:29 -0700

    Modified WordAnalyzer to ignore headers in the CSV file

--- app/src/main/java/com/dubhacks/moderately_modest_group/readingaid/WordAnalyzer.java
+++ app/src/main/java/com/dubhacks/moderately_modest_group/readingaid/WordAnalyzer.java
@@ -1,6 +1,7 @@
 package com.dubhacks.moderately_modest_group.readingaid;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -15,16 +16,23 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by rober on 10/22/2017.
+ * Created by Robert on 10/22/2017.
  */
 
 public class WordAnalyzer {
     public static Map<String, Double> getWordDifficulties(Context context) throws IOException {
         Map<String, Double> wordDifficulties = new HashMap<>();
         InputStream input = context.getResources().openRawResource(R.raw.worddifficulties);
+        // Log.i("Reading Aid", input.toString());
         CSVParser parser = CSVParser.parse(input, StandardCharsets.US_ASCII, CSVFormat.EXCEL);
         List<CSVRecord> records = parser.getRecords();
+        int i = 0;
         for (CSVRecord record : records) {
+            if (i < 1) {
+                i++;
+                continue;
+            }
+
             String word = record.get(0);
             Double ageOfAcquisition;
             try {
@@ -33,6 +41,7 @@ public class WordAnalyzer {
                 ageOfAcquisition = null;
             }
             wordDifficulties.put(word, ageOfAcquisition);
+
         }
 
         return wordDifficulties;
